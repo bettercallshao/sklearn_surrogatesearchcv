b847025a8167fa732a96d801a16891be4ea7dc20
Shaoqing Tan <tim.tan@hydiant.com>
2018-12-09 20:20:24 -0500

    First commit with PyPi skeleton

---
+++ README.rst
@@ -0,0 +1,15 @@
+Surrogate Search CV
+==========================
+
+This package implements a randomized hyper parameter search for sklearn (similar to RandomizedSearchCV) but utilizes surrogate adaptive sampling from pySOT. Use this similarly to RandomizedSearchCV with a few extra paramters.
+
+Credits
+-------
+
+- `Distribute`_
+- `Buildout`_
+- `modern-package-template`_
+
+.. _Buildout: http://www.buildout.org/
+.. _Distribute: http://pypi.python.org/pypi/distribute
+.. _`modern-package-template`: http://pypi.python.org/pypi/modern-package-template
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.1'
+
+install_requires = [
+    'sklearn>=0.20'
+]
+
+
+setup(name='sklearn_surrogatesearchcv',
+    version=version,
+    description="Surrogate adaptive randomized search for hyper parameters fin sklearn.",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+    ],
+    keywords='',
+    author='Shaoqing Tan',
+    author_email='tansq7@gmail.com',
+    url='',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+)
